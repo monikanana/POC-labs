e96a10e7b063210424ff63cd5bf122cae1912fdd
monikanana <monikabaranns@gmail.com>
2018-11-27 14:07:17 +0100

    removed lab07 duplicate

--- lab07 - Filtracja obrazów - bilateralna i non local means/bilateral.m
+++
@@ -1,9 +0,0 @@
-function data_filtered = bilateral(data, local_window)
-    
-    % uchwyt do funkcji 'bilateral_local'
-    FUNCTION = @(data_, local_window_)bilateral_local(data_, local_window_);
-
-    % przekazanie uchwytu FUNCTION do mechanizmu 'confilt'
-    data_filtered = colfilt(data, local_window, 'sliding', FUNCTION, local_window);
-    
-end
